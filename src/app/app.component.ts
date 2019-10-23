e44e64cfd82365ee01e29f6a3015e9fc3a1a4567
dmytro1990 <iavorskyi1990@gmail.com>
2019-10-23 18:42:13 -0400

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Todo {
+  task: string;
+  completed: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+  newTask: string ='';
+  tasks: Todo []= [
+    {completed: false, task: 'Walk the dog.' },
+    {completed: false, task: 'Go to the store.' },
+    {completed: false, task: 'Get gas.'},
+    {completed: false, task: 'Wash the car.'},
+
+  ]
+  filterText: string;
+
+  filteredTasks = [...this.tasks];
+
+  completeTask(task){
+    task.completed = true;
+}
+
+filterTasks(){
+  this.filteredTasks = this.tasks.filter(item => item.task.toLowerCase().includes(this.filterText)
+    );
+    if (!this.filterText) {
+      this.filteredTasks = [...this.tasks];
+    }
+  }
+
+addTask() {
+  const newItem = {
+    task: this.newTask,
+    completed: false
+  };
+  this.tasks.push(newItem);
+  this.newTask = null;
+  this.filteredTasks = [...this.tasks];
+};
+
+removeTask(i){
+this.tasks.splice(i, 1);
+this.filteredTasks = [...this.tasks];
+};
 }
